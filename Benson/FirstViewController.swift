482556f91d1bf01e59443d63bcc24fae1a3fbbff
Aaron Baw <aaronbaw@gmail.com>
2019-08-18 15:29:45 +0200

    Initial Commit

---
+++ Benson/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Benson
+//
+//  Created by Aaron Baw on 18/08/2019.
+//  Copyright © 2019 Ventr. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Benson/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Benson
+//
+//  Created by Aaron Baw on 18/08/2019.
+//  Copyright © 2019 Ventr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

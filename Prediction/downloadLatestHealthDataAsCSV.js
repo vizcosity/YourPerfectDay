7aae183cbeed56db8f4a70e6eb96769cb5b4ba98
Dabba DoubleYoo <dvicbalbaw@gmail.com>
2020-03-24 22:13:58 +0100

    Ensure data response conforms to format {success, data} object pattern

--- Prediction/downloadLatestHealthDataAsCSV.js
+++ Prediction/downloadLatestHealthDataAsCSV.js
@@ -9,7 +9,7 @@ const request = require('request');
 const _SAMPLE_LOCAL_DATA = JSON.parse(fs.readFileSync('./sampleAggHealthAndCheckinData.json').toString('utf8'));
 
 function getHealthData(){ return new Promise((resolve, reject) => {
-    request.get('http://benson-backend.herokuapp.com/api/v1/healthDataAndCheckinsAggregatedBy/day', (req, res) => {
+    request.get(`http://benson-backend.herokuapp.com/api/v1/healthDataAndCheckinsAggregatedBy/${process.argv[2]}`, (req, res) => {
       return resolve(JSON.parse(res.body));
     });
   })
@@ -19,7 +19,10 @@ async function main(healthData){
 
   if (!healthData) healthData = await getHealthData();
 
-  let keys = new Set(Object.keys(healthData.result[0]))
+  // let keys = new Set(Object.keys(healthData.result[0]));
+
+  // Find the result with the largest set of keys.
+  let keys = new Set(healthData.result.map(Object.keys).sort((a, b) => b.length - a.length)[0]);
 
   healthData.result.forEach(result => {
 
